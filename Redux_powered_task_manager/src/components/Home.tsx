2e77606b65c0ab26343ff22aa1da3933c26e45c8
Dawit Andargachew <hereisdawit@gmail.com>
2023-08-12 20:51:57 +0300

    task manager with redux state management

--- Redux_powered_task_manager/src/components/Home.tsx
+++ Redux_powered_task_manager/src/components/Home.tsx
@@ -4,51 +4,45 @@ import { Todo } from "../models/model";
 import { ShowTodo } from "./ShowTodo";
 
 export const Home: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todo, setTodo] = useState<string>("");
+  // const [todos, setTodos] = useState<Todo[]>([]);
   // cons
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo("");
-    }
+    // if (todo) {
+    //   setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+    //   setTodo("");
+    // }
   };
 
   const deleteTask = (id: any) => {
-    const taskList = todos.filter((item) => item.id !== id);
-    setTodos(taskList);
+    // const taskList = todos.filter((item) => item.id !== id);
+    // setTodos(taskList);
   };
   const markasDone = (id: any) => {
-    const taskList = todos.map((item) =>
-      item.id === id ? { ...item, isDone: true } : item
-    );
-    setTodos(taskList);
+    // const taskList = todos.map((item) =>
+    //   item.id === id ? { ...item, isDone: true } : item
+    // );
+    // setTodos(taskList);
   };
 
   const updateTask = (id: any, content: string) => {
-    const taskList = todos.map((item) =>
-      item.id === id ? { ...item, todo: content } : item
-    );
-    setTodos(taskList);
+    // const taskList = todos.map((item) =>
+    //   item.id === id ? { ...item, todo: content } : item
+    // );
+    // setTodos(taskList);
   };
 
-  console.log(todos);
 
   return (
     <div className=" m-4 p-2 ">
       <div className="text-5xl font-mono font-semibold text-center text-gray-300 py-3 my-4">
         Taskfiy
       </div>
-      <Form todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <Form />
 
-      <ShowTodo
-        todos={todos}
-        deleteTask={deleteTask}
-        markasDone={markasDone}
-        updateTask={updateTask}
-      />
+      <ShowTodo />
     </div>
   );
 };
