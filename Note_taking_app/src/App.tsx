173d760531151ea8592e3d6c7feaee7ce17da4be
Dawit Andargachew <hereisdawit@gmail.com>
2023-08-13 15:36:23 +0300

    Initial commit

---
+++ Note_taking_app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import { Home } from './components/Home';
+import { store } from './redux_context/store';
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+
+    <div className="">
+      <Home />
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
